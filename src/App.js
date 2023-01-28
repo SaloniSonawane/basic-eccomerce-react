69e0b01f7eae805800e7009f98c67904b72d8257
SaloniSonawane <salonisonawane18@gmail.com>
2023-01-28 23:46:24 +0530

    ecommerce_products

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Headers from './Components/Header/headers';
 import Slider from './Components/Slider/slider';
 import Footer from './Components/Footer/footer';
+import Products from './Components/Products/products';
 function App() {
   return (
     <>
     <Headers/>
     <Slider/>
+    <Products/>
     <Footer/>
     </>
     
---
+++ src/Components/Products/products.jsx
@@ -0,0 +1,133 @@
+import React from 'react'
+
+export default function Products() {
+  return (
+    <div className='product_main_div'>
+      <h1 className='text-center'>Our Products</h1>
+      <div className='container '>
+        <div className='row'>
+          <div className='col-lg-3 col-sm-6'>
+            <div className='card' style={{ width: "18rem" }}>
+              <img src='img11.avif' className='card-img-top' alt='...' />
+              <div className='card-body'>
+                <h5 className='card-title'>Shoes</h5>
+                <p className='card-text'>
+                 Best Selling 
+                </p>
+              </div>
+             
+              <div className='card-body'>
+                <button href='#' className='card-link  btn-success'>
+                  Add To Card
+                </button>
+                <button href='#' className='card-link  btn-primary'>
+                 View Product
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className='col-lg-3 col-sm-6'>
+            <div className='card' style={{ width: "18rem" }}>
+              <img src='img11.avif' className='card-img-top' alt='...' />
+              <div className='card-body'>
+                <h5 className='card-title'>Shoes</h5>
+                <p className='card-text'>
+                 Best Selling 
+                </p>
+              </div>
+             
+              <div className='card-body'>
+                <button href='#' className='card-link  btn-success'>
+                  Add To Card
+                </button>
+                <button href='#' className='card-link  btn-primary'>
+                 View Product
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className='col-lg-3 col-sm-6'>
+            <div className='card' style={{ width: "18rem" }}>
+              <img src='img11.avif' className='card-img-top' alt='...' />
+              <div className='card-body'>
+                <h5 className='card-title'>Shoes</h5>
+                <p className='card-text'>
+                 Best Selling 
+                </p>
+              </div>
+             
+              <div className='card-body'>
+                <button href='#' className='card-link  btn-success'>
+                  Add To Card
+                </button>
+                <button href='#' className='card-link  btn-primary'>
+                 View Product
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className='col-lg-3 col-sm-6'>
+            <div className='card' style={{ width: "18rem" }}>
+              <img src='img11.avif' className='card-img-top' alt='...' />
+              <div className='card-body'>
+                <h5 className='card-title'>Shoes</h5>
+                <p className='card-text'>
+                 Best Selling 
+                </p>
+              </div>
+             
+              <div className='card-body'>
+                <button href='#' className='card-link  btn-success'>
+                  Add To Card
+                </button>
+                <button href='#' className='card-link  btn-primary'>
+                 View Product
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className='col-lg-3 col-sm-6'>
+            <div className='card' style={{ width: "18rem" }}>
+              <img src='img11.avif' className='card-img-top' alt='...' />
+              <div className='card-body'>
+                <h5 className='card-title'>Shoes</h5>
+                <p className='card-text'>
+                 Best Selling 
+                </p>
+              </div>
+             
+              <div className='card-body'>
+                <button href='#' className='card-link  btn-success'>
+                  Add To Card
+                </button>
+                <button href='#' className='card-link  btn-primary'>
+                 View Product
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className='col-lg-3 col-sm-6'>
+            <div className='card' style={{ width: "18rem" }}>
+              <img src='img11.avif' className='card-img-top' alt='...' />
+              <div className='card-body'>
+                <h5 className='card-title'>Shoes</h5>
+                <p className='card-text'>
+                 Best Selling 
+                </p>
+              </div>
+             
+              <div className='card-body'>
+                <button href='#' className='card-link  btn-success'>
+                  Add To Card
+                </button>
+                <button href='#' className='card-link  btn-primary'>
+                 View Product
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
